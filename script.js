90879b1e12754eccb49472e9dfbd27a034463ab0
Hitesh Marwaha <73687191+hitu69@users.noreply.github.com>
2023-05-02 12:54:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+function firstNonRepeatedChar(str) {
+  const charCounts = new Map();
+  for (const char of str) {
+  charCounts.set(char, (charCounts.get(char) || 0) + 1);
+  }
+    
+  for (const char of str) {
+  if (charCounts.get(char) === 1) {
+  return char;
+  }
+  } 
+  return "There's no non-repeated character in the string.";
+  } 
+  
+  const userInput = prompt("Enter a string:");
+  const result = firstNonRepeatedChar(userInput);
+  if (result) {
+  alert(`The first non-repeated character is '${result}'`);
+  } else {
+  alert("There's no non-repeated character in the string.");
+  }
